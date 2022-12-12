549e7f59b8d87765248228dafa143837d84c2515
Ronald Cifuentes <106139113+rctreinta@users.noreply.github.com>
2022-12-11 23:00:00 -0500

    :technologist: Add Eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import React, {FC} from 'react'
+import React, {FC} from 'react';
 
 export interface IndexProps {
-  dataTestId?: string
+  dataTestId?: string;
 }
 
 const Index: FC<IndexProps> = ({dataTestId = 'index-test'}) => {
   const handleButton = (): void => {
-    console.log('TestXXX')
-  }
+    console.log('TestXXX');
+  };
 
   return (
     <>
       <div data-testid={dataTestId} />
       <button onClick={handleButton}>Testx</button>
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
